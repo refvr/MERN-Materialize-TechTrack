5b9f032b93bd5b3a17644e5df32c3e4311cec2a4
Artyom <lisovtsovv@gmail.com>
2020-10-08 13:53:55 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, { useEffect, Fragment } from "react";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import "materialize-css/dist/css/materialize.min.css";
+import { Provider } from "react-redux";
+import store from "./store";
+import M from "materialize-css/dist/js/materialize.min.js";
+
+import "./App.css";
+import TechListModal from "./components/techs/TechListModal";
+
+const App = () => {
+  useEffect(() => {
+    //initialize MJS
+    M.AutoInit();
+  });
+  return (
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
+  );
+};
+
+export default App;
